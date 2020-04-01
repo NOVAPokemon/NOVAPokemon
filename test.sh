db9cf06fca21e8c3f8a5308201ab53285d17b78b
Bruno Anjos <bruno.vale.anjos@gmail.com>
2020-04-01 12:37:13 +0100

    Fix test script and add new module versions

--- test.sh
+++ test.sh
@@ -1,21 +1,7 @@
 #!/bin/bash
 
-cd authentication
-go test github.com/NOVAPokemon/authentication/...
-
-cd ../battles
-go test github.com/NOVAPokemon/battles/...
-
-cd ../client
-go test github.com/NOVAPokemon/client/...
-
-cd ../generator
-go test github.com/NOVAPokemon/generator/...
-
-cd ../trades
-go test github.com/NOVAPokemon/trades/...
-
-cd ../utils
-go test github.com/NOVAPokemon/utils/...
-
-cd ..
+for d in */ ; do
+  cd "$d" || exit
+  go test github.com/NOVAPokemon/"$d"/...
+  cd .. || exit
+done
