509074b087570758de1db2d6ec8f71d5266cda4a
Bruno Anjos <bruno.vale.anjos@gmail.com>
2020-04-24 16:31:27 +0100

    edit start script to use base image

--- scripts/run_servers.sh
+++ scripts/run_servers.sh
@@ -2,13 +2,16 @@
 
 set -e
 
+DOCKERIZE_VERSION=v0.6.1
+
 cd "$(go env GOPATH)"/src/github.com/NOVAPokemon/ || exit
 
 bash scripts/build_servers.sh
 
-# PORTS
-export MONGO_PORT=27017
+#MONGO
+export MONGODB_URL="mongodb://localhost:27017"
 
+# PORTS
 export AUTH_PORT=8001
 export BATTLES_PORT=8002
 export GYM_PORT=8003
@@ -20,8 +23,6 @@ export TRADES_PORT=8008
 export TRAINERS_PORT=8009
 
 # HOSTNAMES
-export MONGO_NAME="mongo_service"
-
 export AUTH_NAME="authentication"
 export BATTLES_NAME="battles"
 export GYM_NAME="gym"
@@ -42,8 +43,6 @@ export HTTP_PREFIX="http://"
 export TCP_PREFIX="tcp://"
 
 # WAIT FLAGS
-export WAIT_MONGO="$WAIT_FLAG $TCP_PREFIX$MONGO_NAME:$MONGO_PORT"
-
 export WAIT_AUTH="$WAIT_FLAG $HTTP_PREFIX$AUTH_NAME:$AUTH_PORT"
 export WAIT_BATTLES="$WAIT_FLAG $HTTP_PREFIX$BATTLES_NAME:$BATTLES_PORT"
 export WAIT_GYM="$WAIT_FLAG $HTTP_PREFIX$GYM_NAME:$GYM_PORT"
@@ -55,8 +54,28 @@ export WAIT_TRADES="$WAIT_FLAG $HTTP_PREFIX$TRADES_NAME:$TRADES_PORT"
 export WAIT_TRAINERS="$WAIT_FLAG $HTTP_PREFIX$TRAINERS_NAME:$TRAINERS_PORT"
 
 # COMMANDS
+cd base_image || exit
+
+if [ ! -e dockerize ]; then
+  echo "Downloading dockerize"
+  wget https://github.com/jwilder/dockerize/releases/download/v0.6.1/dockerize-linux-amd64-$DOCKERIZE_VERSION.tar.gz \
+   -O dockerize.tar.gz
+  tar -xzvf dockerize.tar.gz
+  rm dockerize.tar.gz
+fi
+
+echo "------------------------------ BUILDING nova-server-base image ------------------------------"
+
+docker build -t nova-server-base:latest .
+
+cd .. || exit
+
+echo "------------------------------ BUILDING images with docker-compose ------------------------------"
 
 docker-compose build
+
+echo "------------------------------ STARTING docker-compose ------------------------------"
+
 docker-compose up --remove-orphan
 
 bash scripts/clean_binaries.sh
