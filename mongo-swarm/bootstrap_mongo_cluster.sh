9aaa5a0304d76d6f834419b04ac13b0351da3194
Nuno <nm.morais@campus.fct.unl.pt>
2020-05-04 11:08:28 +0100

    extract number of replicas to external config file

--- mongo-swarm/bootstrap_mongo_cluster.sh
+++ mongo-swarm/bootstrap_mongo_cluster.sh
@@ -19,7 +19,7 @@ echo "-----Deleting previous bootstrap container-----"
 docker rm -f ${PREFIX}-mongo-cluster-bootstrap || true
 echo "-----Deploying bootstrap container-----"
 docker run -d --network primary_net --name ${PREFIX}-mongo-cluster-bootstrap mongo-cluster-bootstrap $(cat bootstrap_cmd)
-#bash local-status.sh ${PREFIX} ${N_MONGOS} #if removing this comment, then the bootstrap command cannot be detached from
+#bash check_cluster_status.sh ${PREFIX} ${N_MONGOS} #if removing this comment, then the bootstrap command cannot be detached from
 echo "-----Cleaning up-----"
 rm docker-compose.yml || true
 rm bootstrap_cmd || true
