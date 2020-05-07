83e2a8a128ad28bc03f2b2a7c1d60f79acd95693
Bruno Anjos <banjos@localhost.localdomain>
2020-05-07 13:31:29 +0100

    update scripts to ignore mongo-swarm

--- scripts/push.sh
+++ scripts/push.sh
@@ -2,11 +2,12 @@
 
 ignored_scripts="scripts/"
 ignored_base_image="base_image/"
+ignored_mongo_swarm="mongo-swarm/"
 
 for d in */; do
-	if [ "$d" == $ignored_scripts ] || [ "$d" == $ignored_base_image ]; then
-		continue
-	fi
+	if [ "$d" == $ignored_scripts ] || [ "$d" == $ignored_base_image ] || [ "$d" == $ignored_mongo_swarm ]; then
+    continue
+  fi
 	cd "$d" || exit
 	git push
 	cd .. || exit
