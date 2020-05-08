db4eab14951a1fa3ab79d816abd10f596dad52b2
Nuno Morais <nm.morais@campus.fct.unl.pt>
2020-05-08 09:22:53 +0100

    update subchart versions to v2, change install script

--- scripts/install_kube_cluster.sh
+++ scripts/install_kube_cluster.sh
@@ -1,9 +1,10 @@
-#!/bin/bash -ex
+#!/bin/bash
 
 set -e
 
 helm uninstall novapokemon || true
 bash scripts/build_service_images.sh
 cd deployment-chart
+helm dependency update
 helm upgrade --install novapokemon . -f ./values.yaml
 cd ..
