2cd1904bdc247b358a011fef629b7726566986bd
Nuno Morais <nm.morais@campus.fct.unl.pt>
2020-05-12 18:02:18 +0100

    change location servers to statefulSet

--- scripts/install_kube_cluster.sh
+++ scripts/install_kube_cluster.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 set -e
-
 helm uninstall novapokemon || true
 bash scripts/build_service_images.sh
 cd deployment-chart
 helm dependency update
+kubectl taint nodes --all node-role.kubernetes.io/master- || true
 helm upgrade --install novapokemon . -f ./values.yaml
 cd ..
