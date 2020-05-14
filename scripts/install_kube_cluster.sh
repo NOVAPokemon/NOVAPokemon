3295cd38ee1f74006ed0df3a6f73a7afef68644b
Nuno Morais <nm.morais@campus.fct.unl.pt>
2020-05-14 14:43:09 +0100

    add dashboard to install script

--- scripts/install_kube_cluster.sh
+++ scripts/install_kube_cluster.sh
@@ -7,4 +7,5 @@ cd deployment-chart
 helm dependency update
 kubectl taint nodes --all node-role.kubernetes.io/master- || true
 helm upgrade --install novapokemon . -f ./values.yaml
+helm upgrade --install kube-dash stable/kubernetes-dashboard --namespace=kube-system -f ./values.yaml
 cd ..
