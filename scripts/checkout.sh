32284ccd5a42a838cdcfad514aa378f356b605ad
Bruno Anjos <bruno.vale.anjos@gmail.com>
2020-05-18 14:06:16 +0100

    add basic checkout script

---
+++ scripts/checkout.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+ignored_scripts="scripts/"
+ignored_base_image="base_image/"
+ignored_mongo_swarm="mongo-swarm/"
+
+for d in */; do
+	if [ "$d" == $ignored_scripts ] || [ "$d" == $ignored_base_image ] || [ "$d" == $ignored_mongo_swarm ]; then
+    continue
+  fi
+	cd "$d" || exit
+	git checkout master
+	cd .. || exit
+done
