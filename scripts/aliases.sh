0bbf65c4c1dfc2d33b5a81e0c5f3465e179c74dc
Bruno Anjos <bruno.vale.anjos@gmail.com>
2020-05-18 14:05:55 +0100

    add checkout script for all submodules

--- scripts/aliases.sh
+++ scripts/aliases.sh
@@ -1,11 +1,12 @@
-NOVAPOKEMON_DIR="$(go env GOPATH)/src/github.com/NOVAPokemon"
+NOVAPOKEMON_DIR="~/git/NOVAPokemon"
 NOVAPOKEMON_SCRIPTS_DIR="$NOVAPOKEMON_DIR/scripts"
 NOVAPOKEMON_TEST_FILE="test.sh"
 
-cdToDir="cd $HOME/go/src/github.com/NOVAPokemon"
+cdToDir="cd ${NOVAPOKEMON_DIR}"
 
 alias testNOVA="$cdToDir; bash $NOVAPOKEMON_SCRIPTS_DIR/$NOVAPOKEMON_TEST_FILE; cd -"
 alias pullNOVA="$cdToDir; bash $NOVAPOKEMON_SCRIPTS_DIR/pull.sh; cd -"
 alias pushNOVA="$cdToDir; bash $NOVAPOKEMON_SCRIPTS_DIR/push.sh; cd -"
 alias buildNOVA="$cdToDir; bash $NOVAPOKEMON_SCRIPTS_DIR/build_binaries.sh; cd -"
 alias runNOVA="$cdToDir; bash $NOVAPOKEMON_SCRIPTS_DIR/run_servers.sh; cd -"
+alias masterNOVA="$cdToDir; bash $NOVAPOKEMON_SCRIPTS_DIR/checkout.sh; cd -"
