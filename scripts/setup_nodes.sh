0dec3ff06646efabc34efb2c7d93e88a65e739f7
Bruno Anjos <bruno.vale.anjos@gmail.com>
2020-05-25 11:06:55 +0100

    fix labels to support multi context nodes (servers/clients)

--- scripts/setup_nodes.sh
+++ scripts/setup_nodes.sh
@@ -3,13 +3,23 @@
 nodes_out=$(kubectl get nodes --template '{{range .items}}{{.metadata.name}}{{"\n"}}{{end}}')
 readarray -t nodes <<<"$nodes_out"
 
+if [ ${#nodes[@]} -eq 1 ]; then
+  kubectl label nodes "${nodes[0]}" serversnode-
+  kubectl label nodes "${nodes[0]}" clientsnode-
+  kubectl label nodes "${nodes[0]}" serversnode=true
+  kubectl label nodes "${nodes[0]}" clientsnode=true
+
+  exit 1
+fi
 
 for ((i=0;i<((${#nodes[@]}-1));i++))
 do
-  kubectl label nodes "${nodes[$i]}" nodetype-
-  kubectl label nodes "${nodes[$i]}" nodetype=servers
+  kubectl label nodes "${nodes[$i]}" serversnode-
+  kubectl label nodes "${nodes[$i]}" clientsnode-
+  kubectl label nodes "${nodes[$i]}" serversnode=true
 done
 
 lastNode="${nodes[((${#nodes[@]}-1))]}"
-kubectl label nodes "$lastNode" nodetype-
-kubectl label nodes "$lastNode" nodetype=clients
+kubectl label nodes "$lastNode" serversnode-
+kubectl label nodes "$lastNode" clientsnode-
+kubectl label nodes "$lastNode" clientsnode=true
