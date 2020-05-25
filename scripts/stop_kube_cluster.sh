c48b7e9b2516aa50db8913a9cbbf0d295d851538
Nuno <nm.morais@campus.fct.unl.pt>
2020-05-25 16:03:28 +0100

    add sleep

--- scripts/stop_kube_cluster.sh
+++ scripts/stop_kube_cluster.sh
@@ -3,5 +3,14 @@
 set -e
 
 helm uninstall novapokemon
+echo "waiting 1m for pods to finish and collect logs..."
+sleep 15s
+echo "15s..."
+sleep 15s
+echo "30s..."
+sleep 15s
+echo "45s..."
+sleep 15s
+echo "done"
 bash scripts/save_logs.sh
 bash scripts/save_logs_prometheus.sh
