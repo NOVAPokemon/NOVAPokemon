77d4e9e259e402e8ce94f2123141dce3f0930def
Nuno <nm.morais@campus.fct.unl.pt>
2020-05-25 18:02:44 +0100

    add PVCs for prometheus

--- scripts/init.sh
+++ scripts/init.sh
@@ -2,3 +2,5 @@
 # VOLUME
 kubectl apply -f "${HOME}"/git/NOVAPokemon/deployment-chart/persistentVolumes/elasticSearch-pv.yaml
 kubectl apply -f "${HOME}"/git/NOVAPokemon/deployment-chart/persistentVolumes/prometheus-pvs.yaml
+kubectl apply -f "${HOME}"/git/NOVAPokemon/deployment-chart/persistentVolumes/prometheus-alertmanager-pvc.yaml
+kubectl apply -f "${HOME}"/git/NOVAPokemon/deployment-chart/persistentVolumes/prometheus-server-pvc.yaml
