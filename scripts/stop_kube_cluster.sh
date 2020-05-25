0475ecca79eb815ab774fd7f340881959e87e8f7
Bruno Anjos <bruno.vale.anjos@gmail.com>
2020-05-25 11:47:46 +0100

    add more info to stop cluster

--- scripts/stop_kube_cluster.sh
+++ scripts/stop_kube_cluster.sh
@@ -2,5 +2,15 @@
 
 set -e
 
+kubectl delete job novapokemon-tester || true
 helm uninstall novapokemon
+echo "waiting 1m for pods to finish and collect logs..."
+sleep 15s
+echo "15s..."
+sleep 15s
+echo "30s..."
+sleep 15s
+echo "45s..."
+sleep 15s
+echo "done"
 bash scripts/save_logs.sh
