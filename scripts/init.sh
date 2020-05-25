5f6177745e4b5413754ec2cdfe021b9932278f97
Nuno <nm.morais@campus.fct.unl.pt>
2020-05-25 15:44:22 +0100

    rebase

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # VOLUME
-kubectl apply -f "${HOME}"/git/NOVAPokemon/deployment-chart/pv.yaml
+kubectl apply -f "${HOME}"/git/NOVAPokemon/deployment-chart/persistentVolumes/elasticSearch-pv.yaml
+kubectl apply -f "${HOME}"/git/NOVAPokemon/deployment-chart/persistentVolumes/prometheus-pvs.yaml
