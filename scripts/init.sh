1986eb10226e99bf1567c832a850c4e51818e67b
Bruno Anjos <bruno.vale.anjos@gmail.com>
2020-05-27 10:11:33 +0100

    add master node check to init script

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+
+masterNode=$(kubectl get nodes --template '{{range .items}}{{.metadata.name}}{{"\n"}}{{end}}' --selector='node-role.kubernetes.io/master=')
+hostnode=$(hostname)
+
+if [ $masterNode != $hostnode ]; then
+  echo "running this in $hostnode, expected to run in master $masterNode"
+  exit 1
+fi
+
+mkdir /tmp/logs_elastic
+
 # VOLUME
 kubectl apply -f "${HOME}"/git/NOVAPokemon/deployment-chart/pv.yaml
 
