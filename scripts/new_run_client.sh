43307bd2a789698253804eeb9af95178340ee728
Bruno Anjos <bruno.vale.anjos@gmail.com>
2020-05-29 21:32:32 +0100

    update run client scripts

--- scripts/new_run_client.sh
+++ scripts/new_run_client.sh
@@ -2,13 +2,39 @@
 
 set -e
 
+test_run=false
+
+while getopts ":t" opt; do
+  case ${opt} in
+    a)
+      test_run=true
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      exit 1
+      ;;
+  esac
+done
+
+header="-------------------------"
+
+echo "$header DELETING JOBS $header"
+
 for job in $(kubectl get job --template '{{range .items}}{{.metadata.name}}{{"\n"}}{{end}}' | grep tester)
 do
-    echo "deleting $job"
+    echo "Deleting job: $job"
     kubectl delete job ${job}
 done
 
-until ! kubectl get pods --template '{{range .items}}{{.metadata.name}}{{"\n"}}{{end}}' 2>&1 | grep tester
+echo "$header DELETING PVs $header"
+
+for pv in $(kubectl get pv | grep tester)
+do
+    echo "Deleting pv: $pv"
+    kubectl delete pv ${pv}
+done
+
+until ! kubectl get pods --template '{{range .items}}{{.metadata.name}}{{"\n"}}{{end}}' 2>&1 | grep tester 2>&1 1>/dev/null
 do
 	echo "waiting for testers to finish"
 	sleep 2
@@ -23,15 +49,60 @@ groups = list(map(str, groups))
 groups_joined = \" \".join(groups)
 print(groups_joined)")
 
-echo "found groups: $groups_out"
+echo "$header GROUPS CONFIG $header"
+
+echo "Groups in config: $groups_out"
+
+# Job template vars
+var_image_name="VAR_IMAGE_NAME"
+var_client_nums="VAR_CLIENT_NUMS"
+
+# PV template vars
+var_client_pv_name="VAR_CLIENT_PV_NAME"
+var_client_pv_path="VAR_CLIENT_PV_PATH"
+
+client_job_charts="client/clients_charts_applied"
+client_pvs_dirname="client/client_pvs"
+
+mkdir ${client_job_charts}
+mkdir ${client_pvs_dirname}
 
 group_num=0
-for group in $groups_out
+for number_clients in ${groups_out}
 do
-	echo "adding job for group $group_num with $group clients"
-	sed -i "0,/name:.*/{s/name:.*/name: novapokemon-tester-${group_num}-${group}/}" ${jobs_file}
-	sed -i "s/value:.*/value: \"$group\"/1" ${jobs_file}
-	kubectl apply -f client/clientJobs.yaml
+
+    echo "$header GROUP $group_num $header"
+
+    echo "Creating client group PV ($group_num)"
+
+    time=$(date +%d_%m_%Y__%H_%M_%S)
+    dirname="/tmp/client_logs_${time}/novapokemon-tester-${group_num}"
+    mkdir dirname
+
+    client_pv_name="${client_pvs_dirname}/client-group-pv-${group_num}"
+
+    sed "s/${var_client_pv_name}/novapokemon-pv-${group_num}/" | \
+    sed "s|${var_client_pv_path}|${dirname}|" > ${client_pv_name}
+
+    echo "Applying client-group-pv-$group_num"
+
+    if [[ ! ${test_run} ]]
+    then
+        kubectl apply -f ${client_pv_name}
+    fi
+
+	echo "Creating job for client group $group_num with $number_clients clients"
+
+	sed "s/${var_image_name}/novapokemon-tester-${group_num}-${number_clients}/" ${jobs_file} | \
+	sed "s/${var_client_nums}/$number_clients/" ${jobs_file} > client-group-job-${group_num}
+
+    echo "Applying client-group-job-$group_num"
+
+    if [[ ! ${test_run} ]]
+    then
+	    kubectl apply -f client/client-group-job-${group_num}.yaml
+    fi
+
 	group_num=$((group_num+1))
 done
 
