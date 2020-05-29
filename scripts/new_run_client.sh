8fa44bea0e935243c6814bdb91c27a01685eb8a5
Bruno Anjos <bruno.vale.anjos@gmail.com>
2020-05-29 13:50:35 +0100

    add temporary version of new run clients groups

---
+++ scripts/new_run_client.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -e
+
+for job in $(kubectl get pods --template '{{range .items}}{{.metadata.name}}{{"\n"}}{{end}}' | grep tester)
+do
+    echo "deleting $job"
+    kubectl delete pod ${job}
+done
+
+jobs_file="client/clientJobs.yaml"
+
+groups=$(cat client/client_groups.json | python3 -c "\
+import sys, json;\
+groups = json.load(sys.stdin)['groups'];\
+[print(group) for group in groups]")
+
+for group in ${groups}
+do
+	sed -i "s/value:.*/value: $group/1" $jobs_file
+	#kubectl apply -f client/clientJobs.yaml
+done
+
+#bash scripts/build_client.sh
+#kubectl apply -f client/clientJobs.yaml
+
