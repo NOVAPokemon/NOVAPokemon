e74f25df57ac8e24530fd25cfe83051cb9bee1ae
Bruno Anjos <bruno.vale.anjos@gmail.com>
2020-05-29 14:58:09 +0100

    add multiclient binary build stage to build client script

--- scripts/build_client.sh
+++ scripts/build_client.sh
@@ -7,13 +7,20 @@ cd client || exit
 echo "------------------------------ BUILDING $dirname_stripped ------------------------------"
 
 #remove previous binary if already exists
-if [ -e executable ]; then
+if [[ -e executable ]]; then
   rm executable
 fi
 
 # build new binary
 echo "Building binary..."
 GOOS=linux GOARCH=amd64 go build -o executable .
+
+if [[ -e multiclient ]]; then
+  rm multiclient
+fi
+
+echo "Building multi client binary"
+gcc -o multiclient create_thread_clients.c -lpthread
 
 cd .. || exit
 
