ba93e57069c3fd485e04cfb713f3ea85bd6d9277
Bruno Anjos <bruno.vale.anjos@gmail.com>
2020-06-02 11:06:52 +0100

    fix possible wrong variable resolution on remote host

--- scripts/save_client_logs.sh
+++ scripts/save_client_logs.sh
@@ -3,14 +3,14 @@
 clientsnode=$(kubectl get nodes --template '{{range .items}}{{.metadata.name}}{{"\n"}}{{end}}' --selector='clientsnode=true')
 hostname=$(hostname)
 
-logs_dir="/tmp/current_client_logs" 
+logs_dir="/tmp/current_client_logs"
 
 if [[ ${hostname} != "$clientsnode" ]]
 then
 	echo "Running this on node $hostname, instead of $clientsnode. Will ssh and run there."
 	echo "Provide username for ssh:"
 	read -r username
-	started=$(ssh "$username"@"$clientsnode" 'cat $logs_dir/started_at.txt')
+	started=$(ssh "$username"@"$clientsnode" 'cat /tmp/current_client_logs/started_at.txt')
 	ssh "$username"@"$clientsnode" 'cd ~/git/NOVAPokemon && bash scripts/save_client_logs.sh'
 	scp -r "$username"@"$clientsnode":/tmp/client_logs_collected_"$started" /tmp/client_logs_collected_"$started"
 	exit 0
