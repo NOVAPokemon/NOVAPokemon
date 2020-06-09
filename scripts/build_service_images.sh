4e5323d09d0d0d09319e54c9583bbba8cf0ea91b
Nuno Morais <nm.morais@campus.fct.unl.pt>
2020-06-09 18:16:57 +0100

    improve build images script

--- scripts/build_service_images.sh
+++ scripts/build_service_images.sh
@@ -15,15 +15,15 @@ set -e
 test_race=false
 
 while getopts 'r' flag; do
-	case "${flag}" in
-	r)
-		test_race=true
-		;;
-	*)
-		print_usage
-		exit 1
-		;;
-	esac
+  case "${flag}" in
+  r)
+    test_race=true
+    ;;
+  *)
+    print_usage
+    exit 1
+    ;;
+  esac
 done
 
 echo "------------------------------ BUILDING nova-server-base image ------------------------------"
@@ -31,11 +31,11 @@ echo "------------------------------ BUILDING nova-server-base image -----------
 cd base_image
 
 if [[ ! -e dockerize ]]; then
-	echo "Downloading dockerize"
-	wget https://github.com/jwilder/dockerize/releases/download/v0.6.1/dockerize-linux-amd64-"$DOCKERIZE_VERSION".tar.gz \
-		-O dockerize.tar.gz
-	tar -xzvf dockerize.tar.gz
-	rm dockerize.tar.gz
+  echo "Downloading dockerize"
+  wget https://github.com/jwilder/dockerize/releases/download/v0.6.1/dockerize-linux-amd64-"$DOCKERIZE_VERSION".tar.gz \
+    -O dockerize.tar.gz
+  tar -xzvf dockerize.tar.gz
+  rm dockerize.tar.gz
 fi
 
 docker build . -t novapokemon/nova-server-base
@@ -46,59 +46,82 @@ cd ..
 docker_tag=""
 
 if [[ $test_race == true ]]; then
-	docker_tag="race"
+  docker_tag="race"
 else
-	docker_tag="latest"
+  docker_tag="latest"
 fi
 
-#build images
+rm -rf build_logs || true
+mkdir build_logs
+
+echo "------------------------------ BUILDING SERVICE BINARIES ------------------------------"
 for d in */; do
-	dirname_stripped=$(basename "$d")
+  dirname_stripped=$(basename "$d")
 
-	if [[ "$dirname_stripped" == "$ignored_utils" ]] || [[ "$dirname_stripped" == "$ignored_scripts" ]] ||
-		[[ "$dirname_stripped" == "$ignored_client" ]] || [[ "$dirname_stripped" == "$ignored_mongo_swarm" ]] ||
-		[[ "$dirname_stripped" == "$ignored_base_image" ]] || [[ "$dirname_stripped" == "$ignored_deployment_config" ]] ||
-		[[ "$dirname_stripped" == "$ignored_logs" ]]; then
-		continue
-	fi
+  if [[ "$dirname_stripped" == "$ignored_utils" ]] || [[ "$dirname_stripped" == "$ignored_scripts" ]] ||
+    [[ "$dirname_stripped" == "$ignored_client" ]] || [[ "$dirname_stripped" == "$ignored_mongo_swarm" ]] ||
+    [[ "$dirname_stripped" == "$ignored_base_image" ]] || [[ "$dirname_stripped" == "$ignored_deployment_config" ]] ||
+    [[ "$dirname_stripped" == "$ignored_logs" ]] || [[ "$dirname_stripped" == "build_logs" ]]; then
+    continue
+  fi
 
-	echo "------------------------------ BUILDING $dirname_stripped executable ------------------------------"
+  cd "$d" || exit
 
-	cd "$d" || exit
+  #remove previous binary if already exists
+  if [[ -e executable ]]; then
+    rm executable
+  fi
 
-	#remove previous binary if already exists
-	if [[ -e executable ]]; then
-		rm executable
-	fi
-
-	# build new binary
-	race_flag=""
-	if [[ $test_race == true ]]; then
-		export GOOS=""
-		export GOARCH=""
-		race_flag="--race"
-		echo "Building binary with RACE DETECTION..."
-	else
-		export GOOS=linux
-		export GOARCH=amd64
-		echo "Building binary..."
-	fi
-
-	go build $race_flag -v -o executable .
-	echo "done"
-
-	echo "------------------------------ BUILDING $dirname_stripped image ------------------------------"
-
-	docker build . -t novapokemon/"$dirname_stripped":$docker_tag
-	docker push novapokemon/"$dirname_stripped":$docker_tag
-	echo "done"
-
-	#remove binary after building
-	if [[ -e executable ]]; then
-		rm executable
-	fi
-
-	cd .. || exit
+  # build new binary
+  race_flag=""
+  if [[ $test_race == true ]]; then
+    export GOOS=""
+    export GOARCH=""
+    race_flag="--race"
+    echo "Building $dirname_stripped binary with RACE DETECTION..."
+  else
+    export GOOS=linux
+    export GOARCH=amd64
+    echo "Building $dirname_stripped binary..."
+  fi
+  cd ..
 done
+echo "done!"
 
+echo "------------------------------ BUILDING SERVICE IMAGES ------------------------------"
+
+for d in */; do
+  dirname_stripped=$(basename "$d")
+
+  if [[ "$dirname_stripped" == "$ignored_utils" ]] || [[ "$dirname_stripped" == "$ignored_scripts" ]] ||
+    [[ "$dirname_stripped" == "$ignored_client" ]] || [[ "$dirname_stripped" == "$ignored_mongo_swarm" ]] ||
+    [[ "$dirname_stripped" == "$ignored_base_image" ]] || [[ "$dirname_stripped" == "$ignored_deployment_config" ]] ||
+    [[ "$dirname_stripped" == "$ignored_logs" ]] || [[ "$dirname_stripped" == "build_logs" ]]; then
+    continue
+  fi
+
+  cd "$d" || exit
+
+  {
+    echo "Starting build and push of image for service: ${dirname_stripped}"
+    if [[ -e ../build_logs/"$dirname_stripped" ]]; then
+      rm ../build_logs/"$dirname_stripped"
+    fi
+    touch ../build_logs/"$dirname_stripped"
+
+    go build $race_flag -v -o executable .
+    docker build . -t novapokemon/"$dirname_stripped":$docker_tag >../build_logs/"$dirname_stripped"
+    docker push novapokemon/"$dirname_stripped":$docker_tag >../build_logs/"$dirname_stripped"
+    echo "Done building and pushing image for service: ${dirname_stripped}"
+  } &
+
+  cd .. || exit
+done
+echo "Waiting for finish..."
 wait
+
+for f in build_logs/*; do
+  echo ""
+  cat "${f}"
+done
+rm -rf build_logs
