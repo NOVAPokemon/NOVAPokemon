fb4bb3137806a753b391cad2ae8f32cb3487d0d3
Bruno Anjos <bruno.vale.anjos@gmail.com>
2020-06-10 17:26:17 +0100

    fix error in script with time

--- scripts/check_logs_for_data_races.sh
+++ scripts/check_logs_for_data_races.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+time=$(date +%d_%m_%Y__%H_%M_%S)
+
 for pod in $(kubectl get pod --template '{{range .items}}{{.metadata.name}}{{"\n"}}{{end}}'); do
 	if [[ $pod =~ "novapokemon-" ]]
 	then
@@ -9,7 +11,6 @@ for pod in $(kubectl get pod --template '{{range .items}}{{.metadata.name}}{{"\n
 	echo "-------------------------- POD: $pod --------------------------"
 	pod_logs=$(kubectl logs "$pod")
 	result=$( echo "$pod_logs" | grep "WARNING")
-	time=$(date +%d_%m_%Y__%H_%M_%S)
 	dir_name="data_race_logs_$time"
 
 	if [[ $result =~ "WARNING" ]]
