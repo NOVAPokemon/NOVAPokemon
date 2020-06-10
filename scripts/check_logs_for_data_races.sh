98d11efd78ac675223a5b2eb9ac995890cf53dbf
Bruno Anjos <bruno.vale.anjos@gmail.com>
2020-06-10 17:21:34 +0100

    fix bug in script

--- scripts/check_logs_for_data_races.sh
+++ scripts/check_logs_for_data_races.sh
@@ -4,11 +4,15 @@ for pod in $(kubectl get pod --template '{{range .items}}{{.metadata.name}}{{"\n
 	echo "-------------------------- POD: $pod --------------------------"
 	result=$(kubectl logs "$pod" | grep "WARNING")
 	time=$(date +%d_%m_%Y__%H_%M_%S)
-	dir_name="data_race_logs_"$time""
-	mkdir $dir_name
+	dir_name="data_race_logs_$time"
 
 	if [[ $result =~ "WARNING" ]]
 	then
+		if ! [[ -d $dir_name ]]
+		then
+			mkdir "$dir_name"
+		fi
+
 		echo "FOUND DATA RACES IN $pod"
 		kubeclt logs "$pod" > "$dir_name"/"$pod"_data_race.log
 	fi
