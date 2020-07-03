e748993949cac5dc4d41d26c1feaebe44d06bb60
Bruno Anjos <bruno.vale.anjos@gmail.com>
2020-07-03 14:56:11 +0100

    rename script, fix string to look for

--- scripts/check_logs_for_errors.sh
+++ scripts/check_logs_for_errors.sh
@@ -2,6 +2,9 @@
 
 time=$(date +%d_%m_%Y__%H_%M_%S)
 
+error_string="level=error"
+fatal_string="level=fatal"
+
 for pod in $(kubectl get pod --template '{{range .items}}{{.metadata.name}}{{"\n"}}{{end}}'); do
 	if [[ $pod =~ "novapokemon-" ]]; then
 		continue
@@ -10,8 +13,8 @@ for pod in $(kubectl get pod --template '{{range .items}}{{.metadata.name}}{{"\n
 	echo "-------------------------- POD: $pod --------------------------"
 	pod_logs=$(kubectl logs "$pod")
 	result_races=$(echo "$pod_logs" | grep "WARNING")
-	result_error=$(echo "$pod_logs" | grep "error")
-	result_fatal=$(echo "$pod_logs" | grep "fatal")
+	result_error=$(echo "$pod_logs" | grep $error_string)
+	result_fatal=$(echo "$pod_logs" | grep $fatal_string)
 
 	dir_name="error_logs_$time"
 	if [[ $result_races =~ "WARNING" ]]; then
@@ -23,7 +26,7 @@ for pod in $(kubectl get pod --template '{{range .items}}{{.metadata.name}}{{"\n
 		echo "$pod_logs" >"$dir_name"/"$pod"_data_race.log
 	fi
 
-	if [[ $result_error =~ "error" ]]; then
+	if [[ $result_error =~ $error_string ]]; then
 		if ! [[ -d $dir_name ]]; then
 			mkdir "$dir_name"
 		fi
@@ -32,7 +35,7 @@ for pod in $(kubectl get pod --template '{{range .items}}{{.metadata.name}}{{"\n
 		echo "$pod_logs" >"$dir_name"/"$pod"_errors.log
 	fi
 
-	if [[ $result_fatal =~ "fatal" ]]; then
+	if [[ $result_fatal =~ $fatal_string ]]; then
 		if ! [[ -d $dir_name ]]; then
 			mkdir "$dir_name"
 		fi
@@ -56,8 +59,8 @@ for tester in /tmp/current_client_logs/*; do
 		echo "-------------------------- CLIENT: $client_stripped --------------------------"
 
 		result_races=$(grep "WARNING" "$client")
-		result_error=$(grep "error" "$client")
-		result_fatal=$(grep "fatal" "$client")
+		result_error=$(grep $error_string "$client")
+		result_fatal=$(grep $fatal_string "$client")
 
 		if [[ $result_races =~ "WARNING" ]]; then
 			if ! [[ -d $dir_name ]]; then
@@ -72,7 +75,7 @@ for tester in /tmp/current_client_logs/*; do
 			cp "$client" "$dir_name"/$client_data_race_dir/"$client_stripped"_data_race.log
 		fi
 
-		if [[ $result_error =~ "error" ]]; then
+		if [[ $result_error =~ $error_string ]]; then
 			if ! [[ -d $dir_name ]]; then
 				mkdir "$dir_name"
 			fi
@@ -85,7 +88,7 @@ for tester in /tmp/current_client_logs/*; do
 			cp "$client" "$dir_name"/$client_data_race_dir/"$client_stripped"_errors.log
 		fi
 
-		if [[ $result_fatal =~ "fatal" ]]; then
+		if [[ $result_fatal =~ $fatal_string ]]; then
 			if ! [[ -d $dir_name ]]; then
 				mkdir "$dir_name"
 			fi
